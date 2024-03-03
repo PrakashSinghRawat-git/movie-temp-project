a78685b39ee28bc1fdfa617ef6a198775f30265e
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-03-03 14:16:44 +0530

    typo fixedd

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Meta from "./Meta";
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Meta />
+            <Navbar />
+            <main>{children}</main>
+        </>
+    );
+};
+
+export default Layout;
