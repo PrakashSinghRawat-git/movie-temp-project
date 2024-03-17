7b9c806eddbfe32fcde8eb920990aed4e22019c3
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-03-18 00:09:16 +0530

    fixed bg9

--- components/Navbar.js
+++ components/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = () => {
                                             d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                                         />
                                     </svg>
-                                    <span className="sr-only">Search</span>
+                                    <span className="sr-only ml-3">Search</span>
                                 </button>
                                 <div className="relative hidden md:block">
                                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
