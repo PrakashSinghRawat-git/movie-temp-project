a50ea9dfa107e5b73c67f8d09e96cfce41a81c4c
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-03-17 23:36:51 +0530

    fixed bg

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 
     return (
         <nav className="bg-gray-700 w-full ">
-            <div className="bg-white w-full border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+            <div className=" w-full  bg-gray-900 border-gray-700">
                 <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
                     <Link
                         href="/"
@@ -24,7 +24,7 @@ const Navbar = () => {
                         className=" w-full  block md:w-auto"
                         id="navbar-multi-level"
                     >
-                        <ul className="min-w-[200px] sm:min-w-[300px] flex font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse  flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="min-w-[200px] sm:min-w-[300px] flex font-medium p-4 md:p-0 mt-4 border  rounded-lg  md:space-x-8 rtl:space-x-reverse  flex-row md:mt-0 md:border-0 md: bg-gray-800 md:bg-gray-900 border-gray-700">
                             <li
                                 onClick={() => {
                                     setIsDropdownOpen(!isDropdownOpen);
@@ -33,7 +33,7 @@ const Navbar = () => {
                                 <button
                                     id="dropdownNavbarLink"
                                     data-dropdown-toggle="dropdownNavbar"
-                                    className="flex items-center justify-between w-full py-2 px-3 text-gray-200 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                                    className="flex items-center justify-between w-full py-2 px-3  md:border-0  md:p-0 md:w-auto text-white md:hover:text-blue-500 focus:text-white hover:bg-gray-700 md:hover:bg-transparent"
                                 >
                                     Categories &nbsp;
                                     <svg
@@ -56,16 +56,16 @@ const Navbar = () => {
                                 {isDropdownOpen && (
                                     <div
                                         id="dropdownNavbar"
-                                        className="z-10 absolute font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                                        className="z-10 absolute font-normal  divide-y  rounded-lg shadow w-44 bg-gray-700 divide-gray-600"
                                     >
                                         <ul
-                                            className="py-2 text-sm   text-gray-200"
+                                            className="py-2 text-sm   "
                                             aria-labelledby="dropdownLargeButton"
                                         >
                                             <li>
                                                 <a
                                                     href="#"
-                                                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    className="block px-4 py-2 hover:bg-gray-600 hover:text-white"
                                                 >
                                                     Movies
                                                 </a>
@@ -74,7 +74,7 @@ const Navbar = () => {
                                             <li>
                                                 <a
                                                     href="#"
-                                                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    className="block px-4 py-2 hover:bg-gray-600 hover:text-white"
                                                 >
                                                     Web Series
                                                 </a>
@@ -82,7 +82,7 @@ const Navbar = () => {
                                             <li>
                                                 <a
                                                     href="#"
-                                                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    className="block px-4 py-2 hover:bg-gray-600 hover:text-white"
                                                 >
                                                     Dramas{" "}
                                                 </a>
@@ -90,7 +90,7 @@ const Navbar = () => {
                                             <li>
                                                 <a
                                                     href="#"
-                                                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    className="block px-4 py-2 hover:bg-gray-600 hover:text-white"
                                                 >
                                                     Latest Release
                                                 </a>
@@ -98,7 +98,7 @@ const Navbar = () => {
                                             <li>
                                                 <a
                                                     href="#"
-                                                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    className="block px-4 py-2 hover:bg-gray-600 hover:text-white"
                                                 >
                                                     Most Watched
                                                 </a>
@@ -111,7 +111,7 @@ const Navbar = () => {
                             <li className="md:min-w-[75px] mx-auto text-white ">
                                 <Link
                                     href="/login"
-                                    className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0   md:p-0   dark:hover:bg-gray-700   md:dark:hover:bg-transparent"
+                                    className="block py-2 px-3 text-white rounded md:border-0   md:p-0   hover:bg-gray-700   md:hover:bg-transparent"
                                 >
                                     Log In
                                 </Link>
