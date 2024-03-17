62612715d6f497dcf77696b6d7e1da4f5ab5346d
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-03-17 23:58:17 +0530

    fixed bg4

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,6 +26,64 @@ const Navbar = () => {
                     >
                         <ul className="min-w-[200px] sm:min-w-[300px] flex font-medium p-4 md:p-0 mt-4 border  rounded-lg  md:space-x-8 rtl:space-x-reverse  flex-row md:mt-0 md:border-0 md: bg-gray-800 md:bg-gray-900 border-gray-700">
                             <li
+                                className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                                id="navbar-search"
+                            >
+                                <div className="relative mt-3 md:hidden">
+                                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                        <svg
+                                            className="w-4 h-4  text-gray-400"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 20 20"
+                                        >
+                                            <path
+                                                stroke="currentColor"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                strokeWidth={2}
+                                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                            />
+                                        </svg>
+                                    </div>
+                                    <input
+                                        type="text"
+                                        id="search-navbar"
+                                        className="block w-full p-2 ps-10 text-sm   border   rounded-lg    bg-gray-700  border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                                        placeholder="Search..."
+                                    />
+                                </div>
+                                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                                    <li>
+                                        <a
+                                            href="#"
+                                            className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                            aria-current="page"
+                                        >
+                                            Home
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#"
+                                            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                        >
+                                            About
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="#"
+                                            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                        >
+                                            Services
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li
                                 onClick={() => {
                                     setIsDropdownOpen(!isDropdownOpen);
                                 }}
@@ -116,6 +174,14 @@ const Navbar = () => {
                                 )}
                             </li>
 
+                            <li className="md:min-w-[75px] mx-auto text-white ">
+                                <Link
+                                    href="#"
+                                    className="block py-2 px-3 text-white rounded md:border-0   md:p-0   hover:bg-gray-700   md:hover:bg-transparent"
+                                >
+                                    Setting
+                                </Link>
+                            </li>
                             <li className="md:min-w-[75px] mx-auto text-white ">
                                 <Link
                                     href="/login"
--- components/PopularMovie.js
+++ components/PopularMovie.js
@@ -1,14 +1,18 @@
 import MovieCard from "./MovieCard";
 
 const PopularMovie = ({ movies }) => {
-  return (
-    <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4">
-      <h1 className="text-white text-2xl mt-8 mb-5">What's Popular</h1>
-      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-        {movies.map(movie => <MovieCard movie={movie} key={movie.id} />)}
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-gray-700 container max-w-7xl mx-auto pb-10 px-4">
+            <h1 className="text-neutral-200 font-semibold text-2xl mt-8 mb-5">
+                What's Popular
+            </h1>
+            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default PopularMovie;
