34d4fab68aa78131311f833e30c36b8f2fc739de
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-03-18 21:50:54 +0530

    added footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,14 @@
 import Footer from "./Footer";
 import Meta from "./Meta";
 import Navbar from "./Navbar";
-
+import Footer from "./Footer";
 const Layout = ({ children }) => {
     return (
         <>
             <Meta />
             <Navbar />
             <main>{children}</main>
+            <Footer />
         </>
     );
 };
